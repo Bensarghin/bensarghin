1b9d6ae97ca6861ddc3d06976dc32c3a1fbfea68
unknown <hm.bensarghin@gmail.com>
2021-11-22 14:48:54 +0100

    last work on pc

--- app/Http/Controllers/Backoffice/BlogController.php
+++ app/Http/Controllers/Backoffice/BlogController.php
@@ -16,7 +16,7 @@ class BlogController extends Controller
 {
     public function __construct()
     {
-        return $this->middleware('auth:admin');
+        return $this->middleware('auth');
     }
     /**
      * Display a listing of the resource.
